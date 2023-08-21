d0aeb0750a7a0d08722af69335d714c4c3c32b6d
rajan <rajanamrutiya001@gmail.com>
2023-08-21 11:29:48 +0530

    MovieDB_project_typescript

--- login/login_model.js
+++
@@ -1,8 +0,0 @@
-const client = require("../connectionPG");
-
-function userExistance(userName){
-    let query = `select user_name, user_password, role from users where user_name = $1`;
-    return client.query(query, [userName]);
-}
-
-module.exports = {userExistance};
